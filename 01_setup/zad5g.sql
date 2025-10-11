3c1bff7004fe37ae5a60cb06de97439761c10fc5
Roksana Jandura <147600527+roksanajand@users.noreply.github.com>
2025-10-11 20:59:13 +0200

    Update zad5g.sql

---
+++ 01_setup/zad5g.sql
@@ -0,0 +1,5 @@
+SELECT p.imie, p.nazwisko
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+WHERE pe.kwota BETWEEN 1500 AND 3000;
