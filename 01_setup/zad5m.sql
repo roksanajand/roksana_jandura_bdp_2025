82ebc9d7c16c767f8c1f2dcd0a88b80b21f908e2
Roksana Jandura <147600527+roksanajand@users.noreply.github.com>
2025-10-16 08:54:10 +0200

    Update zad5m.sql

--- 01_setup/zad5m.sql
+++ 01_setup/zad5m.sql
@@ -1,4 +1,5 @@
-SELECT SUM(pe.kwota + pr.kwota) AS suma_wynagrodzen
+SELECT
+    SUM(pen.kwota) + COALESCE(SUM(pr.kwota), 0) AS suma_wynagrodzen
 FROM ksiegowosc.wynagrodzenie w
-JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
-JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii;
+LEFT JOIN ksiegowosc.pensja pen ON w.id_pensji = pen.id_pensji
+LEFT JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii;
